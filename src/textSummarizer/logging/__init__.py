c1269a846c2a08192a96eabad51c5d1092ed1c3f
namant <namant@iitbhilai.ac.in>
2023-09-06 23:04:34 +0530

    intsallation done

--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -8,4 +8,11 @@ log_filepath=os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir,exist_ok=True)
 
 
-logging.basicConfig
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_str,
+    handlers=[logging.FileHandler(log_filepath),
+              logging.StreamHandler(sys.stdout)]
+)
+
+logger=logging.getLogger("textSummarizerLogger")
